29be67ed34870667c60a33a7ac1bc86ce64c86f1
fil <filipisee@hotmail.com>
2022-10-31 23:10:34 -0300

    structuring prisma lists

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,12 +1,21 @@
 import Fastify from "fastify";
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient({
+  log: ["query"],
+});
+
+console.log(prisma);
 
 async function bootstrap() {
   const fastify = Fastify({
     logger: true,
   });
 
-  fastify.get("/pools/count", () => {
-    return { count: 10 };
+  fastify.get("/pools/count", async () => {
+    const count = await prisma.pool.count();
+
+    return { count };
   });
 
   await fastify.listen({ port: 3333 });
