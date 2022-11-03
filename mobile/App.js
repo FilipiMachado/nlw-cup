3af4387664f4dd89f238fff4ed2e38d6e910081e
fil <filipisee@hotmail.com>
2022-11-03 19:37:32 -0300

    working on mobile project structure

--- mobile/App.js
+++ mobile/App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from "react-native";
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Hello Worlfgfgfgd!</Text>
+      <Text>Hello World!</Text>
       <StatusBar style="auto" />
     </View>
   );
