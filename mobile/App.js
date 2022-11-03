3ccafc9747922aa104a9411ae02e9c7656cfbdf2
fil <filipisee@hotmail.com>
2022-11-03 20:48:38 -0300

    working on Loading component

--- mobile/App.js
+++ mobile/App.js
@@ -1,11 +1,24 @@
 import { StatusBar } from "expo-status-bar";
-
 import { NativeBaseProvider, Center, Text } from "native-base";
+import {
+  useFonts,
+  Roboto_400Regular,
+  Roboto_500Medium,
+  Roboto_700Bold,
+} from "@expo-google-fonts/roboto";
+
+import { THEME } from "./src/styles/theme";
 
 export default function App() {
+  const [fontsLoaded] = useFonts({
+    Roboto_400Regular,
+    Roboto_500Medium,
+    Roboto_700Bold,
+  });
+
   return (
-    <NativeBaseProvider>
-      <Center flex={1} bgColor="black">
+    <NativeBaseProvider theme={THEME}>
+      <Center flex={1} bgColor="gray.900">
         <Text color="white" fontSize={24}>
           Hello World!
         </Text>
