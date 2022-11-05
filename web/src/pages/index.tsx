eec710cc945efef0e5122489785ff2debf86220f
fil <filipisee@hotmail.com>
2022-11-05 16:22:32 -0300

    styling main page

--- web/src/pages/index.tsx
+++ web/src/pages/index.tsx
@@ -10,7 +10,7 @@ import iconCheckImg from "../assets/check-icon.svg";
 
 export default function Home() {
   return (
-    <div className="max-w-[1124px] h-screen mx-auto grid grid-cols-2 items-center">
+    <div className="max-w-[1124px] h-screen mx-auto grid grid-cols-2 gap-28 items-center">
       <main>
         <Image src={logoImg} alt="logo image" />
 
@@ -28,22 +28,25 @@ export default function Home() {
 
         <form className="mt-10 flex">
           <input
-            className="flex-1 px-6 py-4 rounded"
+            className="flex-1 px-6 py-4 rounded bg-gray-800 border border-gray-600 text-sm"
             type="text"
             required
             placeholder="Qual é o nome do seu bolão?"
           />
-          <button className="bg-yellow-500" type="submit">
+          <button
+            className="bg-yellow-500 px-6 py-4 rounded text-gray-900 font-bold text-sm uppercase hover:bg-yellow-700"
+            type="submit"
+          >
             Criar meu bolão
           </button>
         </form>
 
-        <p>
+        <p className="mt-4 text-sm text-gray-300 leading-relaxed">
           Após criar seu bolão, você receberá um código único que poderá usar
           para convidar outras pessoas 🚀
         </p>
 
-        <div>
+        <div className="mt-10 pt-10 border-t border-gray-600 divide-x flex justify-between">
           <div>
             <Image src={iconCheckImg} alt="" />
             <div>
