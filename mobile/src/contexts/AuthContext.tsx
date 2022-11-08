2349899b4a33718e320f24b5473c6de9739769d5
fil <filipisee@hotmail.com>
2022-11-08 12:26:09 -0300

    added authcontext

---
+++ mobile/src/contexts/AuthContext.tsx
@@ -0,0 +1,35 @@
+import { createContext, ReactNode } from "react";
+
+interface UserProps {
+  name: string;
+  avatarUrl: string;
+}
+
+export interface AuthContextDataProps {
+  user: UserProps;
+  signIn: () => Promise<void>;
+}
+
+interface AuthProviderProps {
+  children: ReactNode;
+}
+
+export const AuthContext = createContext({} as AuthContextDataProps);
+
+export function AuthContextProvider({ children }: AuthProviderProps) {
+  async function signIn() {}
+
+  return (
+    <AuthContext.Provider
+      value={{
+        signIn,
+        user: {
+          name: "Fil",
+          avatarUrl: "https://github.com/FilipiMachado.png",
+        },
+      }}
+    >
+      {children}
+    </AuthContext.Provider>
+  );
+}
