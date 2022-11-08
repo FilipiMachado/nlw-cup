df0a666d7ee8de6462d24c66c790ce125a39e8b7
fil <filipisee@hotmail.com>
2022-11-08 12:14:23 -0300

    finished auth interface

--- mobile/src/screens/SignIn.tsx
+++ mobile/src/screens/SignIn.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Center, Icon } from "native-base";
+import { Center, Text, Icon } from "native-base";
 import { Fontisto } from "@expo/vector-icons";
 
 import Logo from "../assets/logo.svg";
@@ -8,14 +8,20 @@ import { Button } from "../components/Button";
 
 export function SignIn() {
   return (
-    <Center flex={1} bgColor="gray.900">
+    <Center flex={1} bgColor="gray.900" p={7}>
       <Logo width={212} height={40} />
 
       <Button
+        type="SECONDARY"
         title="ENTRAR COM GOOGLE"
         leftIcon={<Icon as={Fontisto} name="google" color="white" size="md" />}
-        type="SECONDARY"
+        mt={12}
       />
+
+      <Text color="white" textAlign="center" mt={4}>
+        Não utilizamos nenhuma informação além {"\n"} do seu e-mail para criação
+        de sua conta.
+      </Text>
     </Center>
   );
 }
