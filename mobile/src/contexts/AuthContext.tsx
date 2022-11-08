573046ccb4cb9a9cc6b6b1d514781ada1982982c
fil <filipisee@hotmail.com>
2022-11-08 12:29:31 -0300

    created useAuth hook

--- mobile/src/contexts/AuthContext.tsx
+++ mobile/src/contexts/AuthContext.tsx
@@ -17,7 +17,9 @@ interface AuthProviderProps {
 export const AuthContext = createContext({} as AuthContextDataProps);
 
 export function AuthContextProvider({ children }: AuthProviderProps) {
-  async function signIn() {}
+  async function signIn() {
+    console.log("Vamos logar!");
+  }
 
   return (
     <AuthContext.Provider
