a9bf4de9d8d25d9a0d1133eb3b8d170d5078b796
fil <filipisee@hotmail.com>
2022-11-08 11:05:48 -0300

    added svg transformer

--- mobile/src/screens/SignIn.tsx
+++ mobile/src/screens/SignIn.tsx
@@ -4,7 +4,7 @@ import { Center, Text } from "native-base";
 export function SignIn() {
   return (
     <Center flex={1} bgColor="gray.900">
-      <Text color="white" fontSize={24}>
+      <Text color="white" fontSize={24} fontFamily="heading">
         SignIn
       </Text>
     </Center>
