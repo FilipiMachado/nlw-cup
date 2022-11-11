b8c42492322cbe4eaadcf76af0d0069b50a07909
fil <filipisee@hotmail.com>
2022-11-11 18:38:52 -0300

    working on New Pool screen

--- mobile/src/screens/New.tsx
+++ mobile/src/screens/New.tsx
@@ -1,10 +1,12 @@
 import { VStack } from "native-base";
+import { Header } from "../components/Header";
 
 export function New() {
-    return (
-        <VStack flex={1} bgColor="gray.900">
-            
-        </VStack>
+  return (
+    <VStack flex={1} bgColor="gray.900">
+      <Header title="Criar novo bolão" />
 
-    );
+      
+    </VStack>
+  );
 }
