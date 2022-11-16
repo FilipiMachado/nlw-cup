b3876e82ede1e442b914b504a7cdfe6e9173113e
fil <filipisee@hotmail.com>
2022-11-16 20:47:45 -0300

    working on new pool page

--- mobile/src/screens/New.tsx
+++ mobile/src/screens/New.tsx
@@ -1,12 +1,33 @@
-import { VStack } from "native-base";
+import { VStack, Heading } from "native-base";
+
+import Logo from "../assets/logo.svg";
+
 import { Header } from "../components/Header";
+import { Input } from "../components/Input";
+import { Button } from "../components/Button";
 
 export function New() {
   return (
     <VStack flex={1} bgColor="gray.900">
       <Header title="Criar novo bolão" />
 
-      
+      <VStack mt={8} mx={5} alignItems="center">
+        <Logo />
+
+        <Heading
+          fontFamily="heading"
+          color="white"
+          fontSize="xl"
+          my={8}
+          textAlign="center"
+        >
+          Crie seu próprio bolão da copa e compartilhe entre amigos!
+        </Heading>
+
+        <Input mb={2} placeholder="Qual é o nome do seu bolão?"></Input>
+
+        <Button title="ABC" />
+      </VStack>
     </VStack>
   );
 }
